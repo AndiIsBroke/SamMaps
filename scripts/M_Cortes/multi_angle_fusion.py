eb526bc5dff34ee70798ec9fbc630a8e9686e9aa
jlegrand62 <jlegrand62@gmail.com>
2019-05-10 17:35:49 +0200

    multi-angle fusion script for M. Cortes

---
+++ scripts/M_Cortes/multi_angle_fusion.py
@@ -0,0 +1,25 @@
+from timagetk.io import imread, imsave
+from timagetk.plugins.fusion import fusion_on_first
+from timagetk.visu.mplt import stack_browser, stack_browser_RGBA
+from timagetk.algorithms.resample import isometric_resampling
+
+root_dir = '/projects/lsfm/multi_view/20190430_M_lti6b-gfp'
+# list_fname = ['20190430_multiangle_top.tif', '20190430_multiangle_45.tif', '20190430_multiangle_90.tif']
+list_fname = ['20190430_multiangle_top.tif', '20190430_multiangle_45.tif']
+list_img = [imread(root_dir+"/"+im) for im in list_fname]
+# top_im = imread(root_dir+"/"+list_fname[0])
+
+from timagetk.plugins.cropping import max_percentile_profiles
+max_percentile_profiles(top_im, n_jobs=1)
+
+
+from timagetk.plugins.cropping import threshold_max_percentile_widget
+crop = threshold_max_percentile_widget(top_im, n_jobs=1)
+
+list_img = [threshold_max_percentile_widget(imread(root_dir+"/"+im)) for im in list_fname]
+
+list_img = [isometric_resampling(imread(root_dir+"/"+im), method='max', option='linear') for im in list_fname]
+
+res_trsf, res_imgs = fusion_on_first(list_img, registration_method='rigid')
+
+stack_browser_RGBA(res_imgs, channel_names=[f for f in list_files], colors=['red', 'green', 'blue'], title="Fusion on first ({}-{})".format(reg_method, av_method))
