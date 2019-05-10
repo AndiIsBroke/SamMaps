21f61f76700af1e4f481bc2062c1d97d36b19f29
jlegrand62 <jlegrand62@gmail.com>
2019-05-10 17:39:05 +0200

    debug 'dir' command overwriting

--- scripts/test/z_stack_registration.py
+++ scripts/test/z_stack_registration.py
@@ -1,12 +1,12 @@
 from timagetk.io import imread, imsave
 from timagetk.plugins.registration import z_stack_registration, apply_z_stack_trsf
-# dir = '/projects/SamMaps/SuperResolution/LSM Airyscan/'
-dir = '/data/Meristems/Carlos/SuperResolution/'
+# root_dir = '/projects/SamMaps/SuperResolution/LSM Airyscan/'
+root_dir = '/data/Meristems/Carlos/SuperResolution/'
 
 base_fname = 'SAM1-gfp-pi-stack-LSM800-Airyscan Processing-standard_{}.tif'
 ref_channel = 'PI_SR'
 float_channels = ['PIN_SR', 'PI_conf', 'PIN_conf']
-image = imread(dir + base_fname.format(ref_channel))
+image = imread(root_dir + base_fname.format(ref_channel))
 
 reg_method = 'rigid'
 reg_stack, z_stack_trsf = z_stack_registration(image, method=reg_method, get_trsf=True)
@@ -24,10 +24,10 @@ stack_browser(reg_stack, title="{} registered z-stack".format(reg_method))
 from timagetk.visu.mplt import stack_browser_RGBA
 stack_browser_RGBA([image, reg_stack], channel_names=['Original', 'Registered'], colors=['red', 'green'], title="Z-stack {} registration".format(reg_method))
 
-imsave(dir + base_fname.format(ref_channel+"_z-stack_reg"), reg_stack)
+imsave(root_dir + base_fname.format(ref_channel+"_z-stack_reg"), reg_stack)
 
 # - Apply this registration to the other channels:
 for float_channel in float_channels:
-    image = imread(dir + base_fname.format(float_channel))
+    image = imread(root_dir + base_fname.format(float_channel))
     reg_image = apply_z_stack_trsf(image, z_stack_trsf)
-    imsave(dir + base_fname.format(float_channel+"_z-stack_reg"), reg_image)
+    imsave(root_dir + base_fname.format(float_channel+"_z-stack_reg"), reg_image)
